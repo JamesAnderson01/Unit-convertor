add77d0dc161667bb4e936e656d39294a7e9f84f
RC0030808 <RC0030808@CRO-ICF07-09>
2018-02-07 13:57:07 +0000

    ju6

--- Unit convertor/Form1.cs
+++ Unit convertor/Form1.cs
@@ -24,24 +24,50 @@ namespace Unit_convertor
  {"Gm", 1e9 }
  };
 
+        Dictionary<string, double> areas = new Dictionary<string, double>()
+ {
+ {"nm²", 1e-18 },
+ {"μm²", 1e-12 },
+ {"mm²", 1e-6 },
+ {"cm²", 1e-4 },
+ {"m²", 1 },
+ {"km²", 1e6 },
+ {"Mm²", 1e12 },
+ {"Gm²", 1e18 }
+ };
+        Dictionary<string, double> volumes = new Dictionary<string, double>()
+ {
+ {"nm³", 1e-27 },
+ {"μm³", 1e-18 },
+ {"mm³", 1e-9 },
+ {"cm³", 1e-4 },
+ {"m³", 1 },
+ {"km³", 1e9 },
+ {"Mm³", 1e18 },
+ {"Gm³", 1e27 }
+ };
+
+
+
         public Form1()
         {
             InitializeComponent();
-            foreach (string k in prefixes.Keys)
+            foreach (string k in areas.Keys)
             {
                 comboBox1.Items.Add(k);
                 comboBox2.Items.Add(k);
             }
             comboBox1.SelectedIndex = 4;
             comboBox2.SelectedIndex = 4;
-            update();
+            update();
+
         }
         void update()
         {
             try
             {
                 double d = double.Parse(textBox1.Text);
-                double d2 = d * prefixes[comboBox1.Text] / prefixes[comboBox2.Text];
+                double d2 = d * areas[comboBox1.Text] / areas[comboBox2.Text];
                 label1.Text = d2.ToString();
             }
             catch
@@ -57,17 +83,20 @@ namespace Unit_convertor
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            update();
+            update();
+
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            update();
+            update();
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            update();
+            update();
+
         }
     }
 }
